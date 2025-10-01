2a926eebcdc1aefd36978d4f55011e1a65d6323f
elMestre <carmesce@gmail.com>
2025-10-01 18:54:38 +0200

    funciones

---
+++ funciones02.py
@@ -0,0 +1,18 @@
+print("- INICIO -")
+
+
+# Definimos una función
+def sum(a, b):
+    print("Entramos en la función")
+    c = a + b
+
+    return c
+
+# Llamamos a la función
+sum(7, 3)
+
+resultado = None
+# Guardamos el DATO resultante de la ejecución para operar con el
+print("El resultado es: ", resultado)
+
+print("FIN")

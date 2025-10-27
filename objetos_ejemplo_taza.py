bc403f5914ad7c14494c58735d2287558b6d271c
elMestre <carmesce@gmail.com>
2025-10-27 19:43:10 +0100

    acceso a atributos

--- objetos_ejemplo_taza.py
+++ objetos_ejemplo_taza.py
@@ -3,30 +3,36 @@ class Taza:
     # Atributo de clase:
     #    Compartido por todos los objetos
     cantidad_de_tazas = 0
+    _SORBO = 50
 
     # Método: es una funcion asociada a un objeto
     def beber(self):
-        print(f"Has bebido de la taza {self.color}")
+        if self.cantidad >= Taza._SORBO:
+            self.cantidad -= Taza._SORBO
+            print(f"Has bebido de la taza {self.color}")
+        else:
+            print(f"NO QUEDA BASTANTE LíQUIDO en {self}")
 
     # En el método init inicializamos los
     #     atributos de instancia obligatorios
-    def __init__(self, color):
+    def __init__(self, color, cantidad):
         Taza.cantidad_de_tazas += 1
         self.color = color
+        self.cantidad = cantidad
         print(f"SE HA CREADO UNA taza {self.color}")
 
     def __str__(self):
-        return f"TAZA {self.color}"
+        return f"TAZA {self.color} CANTIDAD {self.cantidad}ml"
 
 
 # Instanciamos objetos de la clase Taza
 tazas = [
-    Taza("rojo"),
-    Taza("azul"),
-    Taza("verde"),
-    Taza("verde"),
-    Taza("verde"),
-    Taza("verde")
+    Taza("rojo", 300),
+    Taza("azul", 250),
+    Taza("verde", 500),
+    Taza("verde", 100),
+    Taza("verde", 200),
+    Taza("verde", 300)
 ]
 
 print()
@@ -38,6 +44,11 @@ for taza in [tazas[0]]:
     print(taza)
     print("El color de la taza (a) es", taza.color)
     taza.beber()
+    print(taza)
     print(taza.cantidad_de_tazas)
     print("---")
     print()
+
+for i in range(6):
+    tazas[0].beber()
+    print(tazas[0])

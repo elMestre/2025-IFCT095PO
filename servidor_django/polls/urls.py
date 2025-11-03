a967769f8db85deae460f08c507b7cdde260b8c8
elMestre <carmesce@gmail.com>
2025-11-03 20:09:31 +0100

    Django tutorial 03 https://docs.djangoproject.com/es/5.2/intro/tutorial03/#write-views-that-actually-do-something

--- servidor_django/polls/urls.py
+++ servidor_django/polls/urls.py
@@ -3,6 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ejemplo: /polls/
     path("", views.index, name="index"),
+    # ejemplo: /polls/tuberculo
     path("tuberculo", views.patata, name="Le Potato view"),
+
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ejemplo: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ejemplo: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- servidor_django/polls/views.py
+++ servidor_django/polls/views.py
@@ -1,9 +1,34 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hey, como mola, estoy aqui.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 
 def patata(request):
     return HttpResponse("Hey, hola patata")
+
+
+def detail(request, question_id):
+    return HttpResponse("Estas mirando la pregunta nº: %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Resultados de la pregunta %s."
+
+    response += "<ol>"
+    for i in range(question_id):
+        response += f"<li>{i + question_id}</li>"
+    response += "</ol>"
+
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vas a votar la pregunta %s." % question_id)
